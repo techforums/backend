2d07092b9b33b8afa023a5dee230b7966e98c8b0
Drashti <drashti.ardeshna@volansys.com>
2023-05-17 18:55:40 +0530

    updated modules

---
+++ backendsrc/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+// const path = require("path");
+const connectToDatabase = require("./config");
+connectToDatabase();
+const PORT = process.env.port || 8080;
+
+// app.use(express.static(path.join(__dirname, "dist")));
+
+app.use("/api", require("./api"));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "dist", "index.html"));
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
+module.exports = app;
